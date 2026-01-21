679b3a6019344512ee887aa8741167cbf1539473
yaseen177 <yaseen.hussain18@outlook.com>
2026-01-21 22:42:47 +0000

    api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "leicester-eye-clinic.firebaseapp.com",
-    projectId: "leicester-eye-clinic",
-    storageBucket: "leicester-eye-clinic.firebasestorage.app",
-    messagingSenderId: "999920746470",
-    appId: "1:999920746470:web:b013872c496f4719d7a027",
-    measurementId: "G-PGQRC1E6G7"
-  };
+  apiKey: "AIzaSyDkIUvB-KCJ7uP8-Adrw0EiEepHbfRAmZ0",
+  authDomain: "leicester-eye-clinic.firebaseapp.com",
+  projectId: "leicester-eye-clinic",
+  storageBucket: "leicester-eye-clinic.firebasestorage.app",
+  messagingSenderId: "999920746470",
+  appId: "1:999920746470:web:b013872c496f4719d7a027",
+  measurementId: "G-PGQRC1E6G7"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
